3e2c0839f4de64cf045c69494bed369a6a804b98
k_z23n39558 <148705247+kz23n39558@users.noreply.github.com>
2023-10-22 17:19:58 +0200

    Create main.py - Anatoliy Kuznetsovich

---
+++ main.py
@@ -0,0 +1,20 @@
+import tweepy
+
+# Добавьте ваши учетные данные Twitter API
+consumer_key = 'ВАШ_КЛЮЧ_ПОТРЕБИТЕЛЯ'
+consumer_secret = 'ВАШ_СЕКРЕТ_ПОТРЕБИТЕЛЯ'
+access_token = 'ВАШ_ТОКЕН_ДОСТУПА'
+access_token_secret = 'ВАШ_СЕКРЕТНЫЙ_ТОКЕН_ДОСТУПА'
+
+# Аутентификация в Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Создание твита
+tweet = "Привет, это Twitter бот, созданный с помощью Python! #TwitterBot #Python"
+
+# Публикация твита
+api.update_status(tweet)
+
+print("Твит успешно опубликован!")
